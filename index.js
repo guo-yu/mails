f9a11190f03066406ceb1cae2d0e880e41e65a9b
Guo Yu <i@guoyu.me>
2013-11-07 22:39:21 +0800

    0.1.0: add templates

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-var path = require('path'),
+var fs = require('fs'),
+    path = require('path'),
+    juice = require('juice').juiceContent,
     engines = {
         jade: require('jade').renderFile,
         swig: require('swig').render
     };
 
-exports.render = function(tpl, params) {
-    var template = path.join(__dirname, templates, tpl);
+exports.render = function(tpl, params, callback) {
+    var template = path.join(__dirname, 'templates', tpl + '.jade');
     if (fs.existsSync(template)) {
         // local theme
-        return engines[engine](template, params);
+        juice(engines.jade(template, params),{
+            url: 'file://' + template
+        }, callback);
     } else {
         // try fetch local module
-        var moduleDir = path.resolve(__dirname, '../node_modules/', tpl);
+        if (tpl.indexOf('/') > -1) {
+            tpl = { name: tpl.substr(0,tpl.indexOf('/')), file: tpl.substr(tpl.indexOf('/') + 1) }
+        } else {
+            tpl = { name: tpl }
+        }
+        var moduleDir = path.resolve(__dirname, '../node_modules/', tpl.name);
         if (fs.existsSync(moduleDir)) {
             // theme from local modules
             try {
                 var pkg = require(path.join(moduleDir, '/package.json'));
-                if (pkg.mails && pkg.mails.engine && pkg.mails.template) {
-                    return engines[pkg.mails.engine](path.join(moduleDir, pkg.mails.template), params);
+                if (pkg['view engine']) {
+                    // 有没有比较好的方法拿到这个view engine? 在mails模块不依赖它的前提下
+                    juice(pkg['view engine'](template, params),{
+                        url: 'file://' + template
+                    }, callback);
+                    callback(null, engines[pkg.mails.engine](path.join(moduleDir, pkg.mails.template), params));
                 } else {
-                    return null;
+                    callback(new Error('template not existed'));
                 }
             } catch (err) {
-                return null;
+                callback(new Error('template not existed'));
             }
         } else {
-            return null;
+            callback(new Error('template not existed'));
         }
     }
 }
