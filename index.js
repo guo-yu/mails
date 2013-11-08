f18de8449d46bfa7fa3ea732bbf870afa09fef7c
Guo Yu <i@guoyu.me>
2013-11-08 14:12:27 +0800

    0.0.1: merge ink templates inside

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var fs = require('fs'),
     path = require('path'),
     juice = require('juice').juiceContent,
-    engines = {
-        jade: require('jade').renderFile,
-        swig: require('swig').render
-    };
+    swig = require('swig');
 
 exports.render = function(tpl, params, callback) {
     var template = path.join(__dirname, 'templates', tpl + '.jade');
     if (fs.existsSync(template)) {
         // local theme
-        juice(engines.jade(template, params),{
+        juice(swig.compileFile(template)(params),{
             url: 'file://' + template
         }, callback);
     } else {
@@ -27,10 +24,10 @@ exports.render = function(tpl, params, callback) {
                 var pkg = require(path.join(moduleDir, '/package.json'));
                 if (pkg['view engine']) {
                     // 有没有比较好的方法拿到这个view engine? 在mails模块不依赖它的前提下
-                    juice(pkg['view engine'](template, params),{
-                        url: 'file://' + template
-                    }, callback);
-                    callback(null, engines[pkg.mails.engine](path.join(moduleDir, pkg.mails.template), params));
+                    // juice(pkg['view engine'](template, params),{
+                    //     url: 'file://' + template
+                    // }, callback);
+                    // callback(null, engines[pkg.mails.engine](path.join(moduleDir, pkg.mails.template), params));
                 } else {
                     callback(new Error('template not existed'));
                 }
