2ff26022633fa3915c013b98217d1f8de18cdef6
Guo Yu <i@guoyu.me>
2013-12-11 14:25:22 +0800

    0.0.5: exports _render func

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var render = require('./libs/render'),
     mailer = require('./libs/mail'),
     cli = require('./libs/cli').cli;
 
+exports._render = render._render;
 exports.render = render.render;
 exports.send = mailer.send;
 exports.mail = mailer.mail;
