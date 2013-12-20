44d3508c00b5e32773677a7fbbb24e3160218185
Guo Yu <i@guoyu.me>
2013-12-20 16:50:42 +0800

    0.0.6: split func loader, use pluse instead.

--- libs/render.js
+++ libs/render.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     path = require('path'),
     swig = require('swig'),
-    glob = require("glob"),
+    pluse = require('pluse'),
     juice = require('juice').juiceContent;
 
 // environments
@@ -9,84 +9,67 @@ swig.setDefaults({
     cache: false
 });
 
+// dirs
+var dirs = {
+    local: path.resolve(__dirname, '../templates'),
+    parent: path.resolve(__dirname, '../../')
+};
+
 // engines map
 exports._render = function(params, callback) {
-    var engine = params.engine,
-        html = '';
-    if (engine.name === 'jade') {
-        html = engine._engine.renderFile(params.template, params.data);
-    } else if (engine.name === 'swig') {
-        html = engine._engine.compileFile(params.template)(params.data);
-    } else {
-        callback(new Error('template engine not supported'))
-        return false;
-    }
-    juice(html, {
-        url: 'file://' + params.template
+    var engine = params.engine, html;
+    if (engine.name === 'jade') html = engine._engine.renderFile(params.template, params.data);
+    if (engine.name === 'swig') html = engine._engine.compileFile(params.template)(params.data);
+    if (!html) return callback(new Error('template engine is not supported'));
+    return juice(html, { url: 'file://' + params.template }, callback);
+};
+
+// check local theme file
+exports.check = function(file) {
+    if fs.existsSync(path.join(dirs.local, file + '.html')) return path.join(dirs.local, file + '.html');
+    return false;
+};
+
+// shorthand to render local theme
+exports.local = function(file, data, callback) {
+    return exports._render({
+        template: file,
+        data: data,
+        engine: {
+            name: 'swig',
+            _engine: swig
+        }
     }, callback);
-}
+};
+
+// shorthand to render module theme
+exports.theme = function(file, data, engine, callback) {
+    return exports._render({
+        template: file,
+        data: data,
+        engine: engine
+    }, callback);
+};
 
 // render mails html
-exports.render = function(tpl, params, callback) {
-    var localTemplate = path.resolve(__dirname, '../templates', tpl + '.html');
-    if (fs.existsSync(localTemplate)) {
-        // local themes
-        exports._render({
-            template: localTemplate,
-            data: params,
-            engine: {
-                name: 'swig',
-                _engine: swig
-            }
-        }, callback);
-    } else {
-        // try fetch local themes module
-        var template = {
-            name: tpl.indexOf('/') > -1 ? tpl.substr(0, tpl.indexOf('/')) : tpl,
-            file: tpl.indexOf('/') > -1 ? tpl.substr(tpl.indexOf('/') + 1) : null
-        };
-        var moduleDir = path.resolve(__dirname, '../../', template.name);
-        if (fs.existsSync(moduleDir)) {
-            // themes as local modules
-            try {
-                var pkg = require(path.join(moduleDir, '/package.json'));
-                if (pkg['view engine']) {
-                    try {
-                        var engine = require(pkg['view engine']);
-                        if (template.file) {
-                            var file = path.join(moduleDir, template.file);
-                            glob(file + '*', function(err, files) {
-                                if (!err) {
-                                    if (files && files.length > 0) {
-                                        exports._render({
-                                            template: files[0],
-                                            data: params,
-                                            engine: {
-                                                name: pkg['view engine'],
-                                                _engine: engine
-                                            }
-                                        }, callback);
-                                    } else {
-                                        callback(new Error('selected file not found'));
-                                    }
-                                } else {
-                                    callback(err);
-                                }
-                            });
-                        } else {
-                            callback(new Error('which template your want to use ?'));
-                        }
-                    } catch (err) {
-                        callback(new Error('view engine not found'));
-                    }
-                } else {
-                    callback(new Error('template engine not select'));
-                }
-            } catch (err) {
-                callback(new Error('template not existed'));
-            }
-        } else {
-            callback(new Error('template not existed'));
+exports.render = function(template, params, callback) {
+    var local = exports.check(template);
+    if (local) return exports.local(local, params, callback);
+    // try fetch local themes module
+    pluse.load(template, function(err, plugin, file){
+        if (err) return callback(new Error('theme not existed'));
+        if (!plugin) return callback(new Error('theme not found'));
+        if (!plugin['view engine']) return callback(new Error('template engine required'));
+        if (!file) return callback(new Error('template not found'));
+        // if we are going to load a file
+        try {
+            var engine = require(plugin['view engine']);
+            exports.theme(file.dir, params, {
+                name: plugin['view engine'],
+                _engine: engine
+            }, callback);
+        } catch (err) {
+            return callback(new Error('template engine required'));
         }
-    }
+    });
 };
